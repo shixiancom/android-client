bf5ad36dc4ccbbc741f2cd1e8a27ef08ff91906f
s0ng <806355705@qq.com>
2015-02-08 22:25:25 +0800

    全局常量类

---
+++ app/src/main/java/com/shixian/android/client/contants/AppContants.java
@@ -0,0 +1,12 @@
+package com.shixian.android.client.contants;
+
+/**
+ * Created by s0ng on 2015/2/8.
+ * 用于保存全局常量
+ */
+public interface AppContants {
+
+    //调试的时候为真 输出log
+    boolean DEBUG=true;
+
+}
